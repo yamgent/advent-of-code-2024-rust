2b3ee5878bf1c633233fd24aed2afc08d518800a
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-12-02 20:32:05 +0800

    Solve day 02 part 1

---
+++ src/bin/day02/main.rs
@@ -0,0 +1,72 @@
+const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2024/02/input.txt");
+
+fn is_safe_increasing(values: &[i32]) -> bool {
+    values
+        .iter()
+        .zip(values.iter().skip(1))
+        .all(|(a, b)| b - a >= 1 && b - a <= 3)
+}
+
+fn is_safe_decreasing(values: &[i32]) -> bool {
+    values
+        .iter()
+        .zip(values.iter().skip(1))
+        .all(|(a, b)| a - b >= 1 && a - b <= 3)
+}
+
+fn p1(input: &str) -> String {
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|val| val.parse::<i32>().expect("a number"))
+                .collect::<Vec<_>>()
+        })
+        .filter(|line| is_safe_increasing(line) || is_safe_decreasing(line))
+        .count()
+        .to_string()
+}
+
+fn p2(input: &str) -> String {
+    let _input = input.trim();
+    "".to_string()
+}
+
+fn main() {
+    println!("{}", p1(ACTUAL_INPUT));
+    println!("{}", p2(ACTUAL_INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE_INPUT: &str = r"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+    #[test]
+    fn test_p1_sample() {
+        assert_eq!(p1(SAMPLE_INPUT), "2");
+    }
+
+    #[test]
+    fn test_p1_actual() {
+        assert_eq!(p1(ACTUAL_INPUT), "421");
+    }
+
+    #[test]
+    fn test_p2_sample() {
+        assert_eq!(p2(SAMPLE_INPUT), "");
+    }
+
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn test_p2_actual() {
+        assert_eq!(p2(ACTUAL_INPUT), "");
+    }
+}
