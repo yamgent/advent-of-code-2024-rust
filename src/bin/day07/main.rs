67e928539d0bf0b96dd31a81cc7d5abd09239190
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-12-07 14:03:42 +0800

    Day 07: Update total_digits() calculation
    Time in local back from 0.09s -> 0.11s, but we get cleaner code in
    return.

--- src/bin/day07/main.rs
+++ src/bin/day07/main.rs
@@ -1,46 +1,10 @@
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2024/07/input.txt");
 
 fn total_digits(value: u64) -> usize {
-    if value < 10 {
+    if value == 0 {
         1
-    } else if value < 100 {
-        2
-    } else if value < 1_000 {
-        3
-    } else if value < 10_000 {
-        4
-    } else if value < 100_000 {
-        5
-    } else if value < 1_000_000 {
-        6
-    } else if value < 10_000_000 {
-        7
-    } else if value < 100_000_000 {
-        8
-    } else if value < 1_000_000_000 {
-        9
-    } else if value < 10_000_000_000 {
-        10
-    } else if value < 100_000_000_000 {
-        11
-    } else if value < 1_000_000_000_000 {
-        12
-    } else if value < 10_000_000_000_000 {
-        13
-    } else if value < 100_000_000_000_000 {
-        14
-    } else if value < 1_000_000_000_000_000 {
-        15
-    } else if value < 10_000_000_000_000_000 {
-        16
-    } else if value < 100_000_000_000_000_000 {
-        17
-    } else if value < 1_000_000_000_000_000_000 {
-        18
-    } else if value < 10_000_000_000_000_000_000 {
-        19
     } else {
-        20
+        (value.ilog10() + 1) as usize
     }
 }
 
