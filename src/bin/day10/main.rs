91910c4ebf3b774512e4fc714b4a9a4a90bd2817
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-12-10 20:56:28 +0800

    Solve day 10 part 2

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -2,8 +2,8 @@ use ahash::{HashSet, HashSetExt};
 
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2024/10/input.txt");
 
-fn p1(input: &str) -> String {
-    let grid = input
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
+    input
         .trim()
         .lines()
         .map(|line| {
@@ -15,9 +15,13 @@ fn p1(input: &str) -> String {
                         ch.to_digit(10).expect("either a dot, or a digit")
                     }
                 })
-                .collect::<Vec<_>>()
+                .collect()
         })
-        .collect::<Vec<_>>();
+        .collect()
+}
+
+fn p1(input: &str) -> String {
+    let grid = parse_input(input);
 
     fn get_score(grid: &[Vec<u32>], x: usize, y: usize) -> usize {
         if grid[y][x] != 0 {
@@ -66,8 +70,51 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    let grid = parse_input(input);
+
+    fn get_rating(grid: &[Vec<u32>], x: usize, y: usize) -> usize {
+        if grid[y][x] != 0 {
+            0
+        } else {
+            let mut acc = 0;
+
+            fn visit(grid: &[Vec<u32>], x: usize, y: usize, acc: &mut usize) {
+                let current_val = grid[y][x];
+                let next_val = current_val + 1;
+
+                if current_val == 9 {
+                    *acc += 1;
+                } else {
+                    if x > 0 && grid[y][x - 1] == next_val {
+                        visit(grid, x - 1, y, acc);
+                    }
+                    if y > 0 && grid[y - 1][x] == next_val {
+                        visit(grid, x, y - 1, acc);
+                    }
+                    if x + 1 < grid[y].len() && grid[y][x + 1] == next_val {
+                        visit(grid, x + 1, y, acc);
+                    }
+                    if y + 1 < grid.len() && grid[y + 1][x] == next_val {
+                        visit(grid, x, y + 1, acc);
+                    }
+                }
+            }
+
+            visit(grid, x, y, &mut acc);
+            acc
+        }
+    }
+
+    (0..grid.len())
+        .into_iter()
+        .map(|y| {
+            (0..grid[y].len())
+                .into_iter()
+                .map(|x| get_rating(&grid, x, y))
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 fn main() {
@@ -78,8 +125,6 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    const SAMPLE_INPUT: &str = r"";
 
     #[test]
     fn test_p1_sample() {
@@ -150,12 +195,58 @@ mod tests {
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+        assert_eq!(
+            p2(r"
+.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....
+"),
+            "3"
+        );
+        assert_eq!(
+            p2(r"
+..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+"),
+            "13"
+        );
+        assert_eq!(
+            p2(r"
+012345
+123456
+234567
+345678
+4.6789
+56789.
+"),
+            "227"
+        );
+        assert_eq!(
+            p2(r"
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+"),
+            "81"
+        );
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "1225");
     }
 }
