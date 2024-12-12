63e9292c0b4c16122d2acd73f28a69c9e87b9697
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-12-12 21:16:32 +0800

    Refactor day 12

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -2,143 +2,13 @@ use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
 
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2024/12/input.txt");
 
-fn p1(input: &str) -> String {
-    let grid = input
-        .trim()
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let mut region_ids = (0..grid.len())
-        .map(|y| (0..grid[y].len()).map(|_| -1).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let mut next_id = 1;
-
-    (0..grid.len()).for_each(|y| {
-        (0..grid[y].len()).for_each(|x| {
-            if region_ids[y][x] == -1 {
-                let current_id = next_id;
-                let current_grid_letter = grid[y][x];
-                next_id += 1;
-
-                fn flood_fill(
-                    grid: &[Vec<char>],
-                    region_ids: &mut Vec<Vec<i32>>,
-                    current_id: i32,
-                    current_grid_letter: char,
-                    x: usize,
-                    y: usize,
-                ) {
-                    region_ids[y][x] = current_id;
-
-                    fn suitable(
-                        grid: &[Vec<char>],
-                        region_ids: &[Vec<i32>],
-                        current_grid_letter: char,
-                        pos: &(usize, usize),
-                    ) -> bool {
-                        let x = pos.0;
-                        let y = pos.1;
-                        region_ids[y][x] == -1 && grid[y][x] == current_grid_letter
-                    }
-
-                    if x > 0 {
-                        let left = (x - 1, y);
-                        if suitable(grid, region_ids, current_grid_letter, &left) {
-                            flood_fill(
-                                grid,
-                                region_ids,
-                                current_id,
-                                current_grid_letter,
-                                left.0,
-                                left.1,
-                            );
-                        }
-                    }
-                    if x + 1 < region_ids[y].len() {
-                        let right = (x + 1, y);
-                        if suitable(grid, region_ids, current_grid_letter, &right) {
-                            flood_fill(
-                                grid,
-                                region_ids,
-                                current_id,
-                                current_grid_letter,
-                                right.0,
-                                right.1,
-                            );
-                        }
-                    }
-                    if y > 0 {
-                        let up = (x, y - 1);
-                        if suitable(grid, region_ids, current_grid_letter, &up) {
-                            flood_fill(
-                                grid,
-                                region_ids,
-                                current_id,
-                                current_grid_letter,
-                                up.0,
-                                up.1,
-                            )
-                        }
-                    }
-                    if y + 1 < region_ids.len() {
-                        let down = (x, y + 1);
-                        if suitable(grid, region_ids, current_grid_letter, &down) {
-                            flood_fill(
-                                grid,
-                                region_ids,
-                                current_id,
-                                current_grid_letter,
-                                down.0,
-                                down.1,
-                            );
-                        }
-                    }
-                }
-
-                flood_fill(
-                    &grid,
-                    &mut region_ids,
-                    current_id,
-                    current_grid_letter,
-                    x,
-                    y,
-                );
-            }
-        });
-    });
-
-    let mut areas: HashMap<i32, usize> = HashMap::new();
-    let mut perimeters: HashMap<i32, usize> = HashMap::new();
-
-    region_ids.iter().enumerate().for_each(|(y, lines)| {
-        lines.iter().enumerate().for_each(|(x, id)| {
-            *areas.entry(*id).or_default() += 1;
-
-            if x == 0 || region_ids[y][x - 1] != *id {
-                *perimeters.entry(*id).or_default() += 1;
-            }
-            if y == 0 || region_ids[y - 1][x] != *id {
-                *perimeters.entry(*id).or_default() += 1;
-            }
-            if x + 1 >= region_ids[y].len() || region_ids[y][x + 1] != *id {
-                *perimeters.entry(*id).or_default() += 1;
-            }
-            if y + 1 >= region_ids.len() || region_ids[y + 1][x] != *id {
-                *perimeters.entry(*id).or_default() += 1;
-            }
-        });
-    });
-
-    areas
-        .into_iter()
-        .map(|(id, area)| *perimeters.get(&id).unwrap_or(&0) * area)
-        .sum::<usize>()
-        .to_string()
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Pricing {
+    ByPerimeter,
+    BySides,
 }
 
-fn p2(input: &str) -> String {
+fn solve(input: &str, pricing: Pricing) -> usize {
     let grid = input
         .trim()
         .lines()
@@ -247,6 +117,7 @@ fn p2(input: &str) -> String {
 
     let mut areas: HashMap<i32, usize> = HashMap::new();
     let mut perimeters: HashMap<i32, usize> = HashMap::new();
+    let mut sides: HashMap<i32, usize> = HashMap::new();
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
     enum Fence {
@@ -265,6 +136,7 @@ fn p2(input: &str) -> String {
 
             if x == 0 || region_ids[y][x - 1] != *id {
                 fences.entry((x, y)).or_default().insert((*id, Fence::Left));
+                *perimeters.entry(*id).or_default() += 1;
 
                 if y == 0
                     || !fences
@@ -272,11 +144,12 @@ fn p2(input: &str) -> String {
                         .unwrap_or(&empty_hashset)
                         .contains(&(*id, Fence::Left))
                 {
-                    *perimeters.entry(*id).or_default() += 1;
+                    *sides.entry(*id).or_default() += 1;
                 }
             }
             if y == 0 || region_ids[y - 1][x] != *id {
                 fences.entry((x, y)).or_default().insert((*id, Fence::Top));
+                *perimeters.entry(*id).or_default() += 1;
 
                 if x == 0
                     || !fences
@@ -284,7 +157,7 @@ fn p2(input: &str) -> String {
                         .unwrap_or(&empty_hashset)
                         .contains(&(*id, Fence::Top))
                 {
-                    *perimeters.entry(*id).or_default() += 1;
+                    *sides.entry(*id).or_default() += 1;
                 }
             }
             if x + 1 >= region_ids[y].len() || region_ids[y][x + 1] != *id {
@@ -292,6 +165,7 @@ fn p2(input: &str) -> String {
                     .entry((x, y))
                     .or_default()
                     .insert((*id, Fence::Right));
+                *perimeters.entry(*id).or_default() += 1;
 
                 if y == 0
                     || !fences
@@ -299,7 +173,7 @@ fn p2(input: &str) -> String {
                         .unwrap_or(&empty_hashset)
                         .contains(&(*id, Fence::Right))
                 {
-                    *perimeters.entry(*id).or_default() += 1;
+                    *sides.entry(*id).or_default() += 1;
                 }
             }
             if y + 1 >= region_ids.len() || region_ids[y + 1][x] != *id {
@@ -307,6 +181,7 @@ fn p2(input: &str) -> String {
                     .entry((x, y))
                     .or_default()
                     .insert((*id, Fence::Bottom));
+                *perimeters.entry(*id).or_default() += 1;
 
                 if x == 0
                     || !fences
@@ -314,17 +189,30 @@ fn p2(input: &str) -> String {
                         .unwrap_or(&empty_hashset)
                         .contains(&(*id, Fence::Bottom))
                 {
-                    *perimeters.entry(*id).or_default() += 1;
+                    *sides.entry(*id).or_default() += 1;
                 }
             }
         });
     });
 
-    areas
+    let measurements = match pricing {
+        Pricing::ByPerimeter => perimeters,
+
+        Pricing::BySides => sides,
+    };
+
+    measurements
         .into_iter()
-        .map(|(id, area)| *perimeters.get(&id).unwrap_or(&0) * area)
+        .map(|(id, measurement)| *areas.get(&id).unwrap_or(&0) * measurement)
         .sum::<usize>()
-        .to_string()
+}
+
+fn p1(input: &str) -> String {
+    solve(input, Pricing::ByPerimeter).to_string()
+}
+
+fn p2(input: &str) -> String {
+    solve(input, Pricing::BySides).to_string()
 }
 
 fn main() {
